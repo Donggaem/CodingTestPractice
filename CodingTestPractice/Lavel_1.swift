544a9739cd7ff0d8b76baac152c9ed28cbbfcc52
Donggaem <ehdrua465111@naver.com>
2023-04-06 20:19:34 +0900

    Lavel_1: 평균 구하기, 짝수와 홀수, 약수의 합, 자릿수 더하기

---
+++ CodingTestPractice/Lavel_1.swift
@@ -0,0 +1,23 @@
+//
+//  Lavel_1.swift
+//  CodingTestPractice
+//
+//  Created by 김동겸 on 2023/04/06.
+//
+
+import Foundation
+//MARK: - 0406
+//MARK: - Lavel_1: 평균 구하기
+//func solution(_ arr:[Int]) -> Double {Double(arr.reduce(0, +)) / Double(arr.count)}
+
+//MARK: - Lavel_1: 짝수와 홀수
+//func solution(_ num:Int) -> String {num % 2 == 0 ? "Even" : "Odd"}
+
+//MARK: - 약수의 합
+//func solution(_ n:Int) -> Int {n != 0 ? (1...n).filter{n % $0 == 0}.reduce(0, +) : 0}
+//MARK: - 자릿수 더하기
+//func solution(_ n:Int) -> Int
+//{
+//    var answer:Int = String(n).map{Int(String($0))!}.reduce(0, +)
+//    return answer
+//}
