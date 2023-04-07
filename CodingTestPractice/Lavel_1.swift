94e2f0b2801f8943383282939850e53d91685d5a
Donggaem <ehdrua465111@naver.com>
2023-04-07 21:57:27 +0900

    Lavel_1: x만큼 간격이 있는 n개의 숫자, 나머지가 1이 되는 수 찾기, 문자열 내 p와 y의 개수, 자연수 뒤집어 배열로 만들기, 정수 내림차순으로 배치하기

--- CodingTestPractice/Lavel_1.swift
+++ CodingTestPractice/Lavel_1.swift
@@ -13,11 +13,65 @@ import Foundation
 //MARK: - Lavel_1: 짝수와 홀수
 //func solution(_ num:Int) -> String {num % 2 == 0 ? "Even" : "Odd"}
 
-//MARK: - 약수의 합
+//MARK: - Lavel_1: 약수의 합
 //func solution(_ n:Int) -> Int {n != 0 ? (1...n).filter{n % $0 == 0}.reduce(0, +) : 0}
-//MARK: - 자릿수 더하기
+//MARK: - Lavel_1: 자릿수 더하기
 //func solution(_ n:Int) -> Int
 //{
 //    var answer:Int = String(n).map{Int(String($0))!}.reduce(0, +)
 //    return answer
 //}
+
+//MARK: - 0407
+//MARK: - Lavel_1: x만큼 간격이 있는 n개의 숫자
+//func solution(_ x:Int, _ n:Int) -> [Int64] {
+//    var result: [Int64] = []
+//    var num: Int = 0
+//    for _ in 1...n {
+//        num += x
+//        result.append(Int64(num))
+//    }
+//    return result
+//}
+
+//다른사람 풀이
+//func solution(_ x:Int, _ n:Int) -> [Int64] {
+//    return Array(1...n).map { Int64($0 * x) }
+//}
+
+//MARK: - Lavel_1: 나머지가 1이 되는 수 찾기
+//func solution(_ n:Int) -> Int {(1...n).filter{n % $0 == 1}.min()!}
+
+//MARK: - Lavel_1: 문자열 내 p와 y의 개수
+//func solution(_ s:String) -> Bool
+//{
+//    var ans:Bool = false
+//    var arr_p: [String] = []
+//    var arr_y: [String] = []
+//    arr_p = s.lowercased().map{String($0)}.filter{$0 == "p"}
+//    arr_y = s.lowercased().map{String($0)}.filter{$0 == "y"}
+//
+//    if s.lowercased().contains("p") || s.lowercased().contains("y"){
+//        if arr_p.count == arr_y.count {
+//            ans = true
+//        } else {
+//            ans = false
+//        }
+//    }else {
+//        ans = true
+//    }
+//    return ans
+//}
+
+//다른사람 풀이
+//func solution(_ s:String) -> Bool
+//{
+//    let string = s.lowercased()
+//    return string.components(separatedBy: "p").count == string.components(separatedBy: "y").count
+//}
+
+//MARK: - Lavel_1: 자연수 뒤집어 배열로 만들기
+//func solution(_ n:Int64) -> [Int] {String(n).map{String($0)}.reversed().map{Int($0)!}}
+
+//MARK: - Lavel_1: 정수 내림차순으로 배치하기
+//func solution(_ n:Int64) -> Int64 {Int64(String(n).map{String($0)}.sorted(by: >).joined())!}
