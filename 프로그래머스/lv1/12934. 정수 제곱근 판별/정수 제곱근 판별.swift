1d6df675be864893048f6102ac657c182cdb37ff
Donggaem <86482563+Donggaem@users.noreply.github.com>
2023-09-01 12:37:13 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.swift
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.swift
@@ -1,5 +1,5 @@
 import Foundation
 func solution(_ n:Int64) -> Int64 {
     let sq = Int64(sqrt(Double(n)))
-    return sq * sq == n ? (sq + 1)*(sq + 1) : -1
+    return sq * sq == n ? Int64(pow(Double(sq + 1), 2)) : -1
 }
