00526649d5be8299e236842b0273842925bd0231
Donggaem <86482563+Donggaem@users.noreply.github.com>
2023-10-06 14:23:04 +0900

    [unrated] Title: l로 만들기, Time: 82.93 ms, Memory: 17.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.swift
@@ -0,0 +1,14 @@
+func solution(_ myString:String) -> String {
+    return myString.replacingOccurrences(of: "a", with: "l")
+        .replacingOccurrences(of: "b", with: "l")
+        .replacingOccurrences(of: "c", with: "l")
+        .replacingOccurrences(of: "d", with: "l")
+        .replacingOccurrences(of: "e", with: "l")
+        .replacingOccurrences(of: "f", with: "l")
+        .replacingOccurrences(of: "g", with: "l")
+        .replacingOccurrences(of: "h", with: "l")
+        .replacingOccurrences(of: "i", with: "l")
+        .replacingOccurrences(of: "j", with: "l")
+        .replacingOccurrences(of: "k", with: "l")
+
+}
