762e5e63290b757df6eb89b5b3f8ca023d8183e4
Donggaem <86482563+Donggaem@users.noreply.github.com>
2023-10-06 13:19:58 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.swift
@@ -0,0 +1,10 @@
+func solution(_ num_list:[Int]) -> [Int] {
+    var result: [Int] = num_list
+    
+    if num_list.last! > num_list[num_list.count - 2] {
+        result.append(num_list.last! - num_list[num_list.count - 2])
+    }else {
+        result.append(num_list.last! * 2)
+    }
+    return result
+}
