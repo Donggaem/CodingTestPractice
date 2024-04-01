a73f878474a64164600bd87a6da30b8b8cdd7d6e
Donggaem <86482563+Donggaem@users.noreply.github.com>
2024-04-01 16:49:13 +0900

    [level 0] Title: 문자열 섞기, Time: 0.13 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
@@ -1,12 +1,16 @@
+// func solution(_ str1:String, _ str2:String) -> String {
+//   var str1Array: [String] = str1.map({String($0)})
+//   var str2Array: [String] = str2.map({String($0)})
+//   var resultArray: [String] = []
+  
+//   for i in 0..<str1.count {
+//     resultArray.append(str1Array[i])
+//     resultArray.append(str2Array[i])
+//   }
+  
+//   return resultArray.joined()
+// }
+
 func solution(_ str1:String, _ str2:String) -> String {
-  var str1Array: [String] = str1.map({String($0)})
-  var str2Array: [String] = str2.map({String($0)})
-  var resultArray: [String] = []
-  
-  for i in 0..<str1.count {
-    resultArray.append(str1Array[i])
-    resultArray.append(str2Array[i])
-  }
-  
-  return resultArray.joined()
+    return zip(str1, str2).map { String($0) + String($1) }.joined()
 }
