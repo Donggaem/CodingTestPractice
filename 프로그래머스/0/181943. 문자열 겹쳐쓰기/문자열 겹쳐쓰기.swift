5831f6f4762cf81f3dd49b99b517ded9c0933b1a
Donggaem <86482563+Donggaem@users.noreply.github.com>
2024-04-02 21:35:06 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.swift
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.swift
@@ -1,7 +1,7 @@
 func solution(_ my_string:String, _ overwrite_string:String, _ s:Int) -> String {
-  var start = String(my_string.prefix(s))
+  var start = my_string.prefix(s)
   var middle = overwrite_string
-  var end = String(my_string.suffix(my_string.count - (overwrite_string.count + s)))
+  var end = my_string.suffix(my_string.count - (overwrite_string.count + s))
   
   return start + middle + end
 }
