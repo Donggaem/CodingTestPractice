ccf0e7b94dcfb77e499752a38401e62bfffaa83d
Donggaem <86482563+Donggaem@users.noreply.github.com>
2024-04-08 15:53:38 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 39.49 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
@@ -1,7 +1,17 @@
+// func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
+//   var result = arr
+//   queries.forEach{ temp in
+//     (temp[0]...temp[1]).filter{ $0 % temp[2] == 0}.forEach{ result[$0] += 1}
+//   }
+//     return result
+// }
+
+//다른사람 풀이
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
-  var result = arr
-  queries.forEach{ temp in
-    (temp[0]...temp[1]).filter{ $0 % temp[2] == 0}.forEach{ result[$0] += 1}
-  }
-    return result
+    var arr = arr
+    queries.forEach { 
+        let (s, e, k) = ($0[0], $0[1], $0[2])
+        (s...e).forEach { if $0 % k == 0 { arr[$0] += 1 } }
+    }
+    return arr
 }
