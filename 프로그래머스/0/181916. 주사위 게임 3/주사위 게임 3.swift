06101e0244db15dfec4340f74a30b227aecdc5bb
Donggaem <86482563+Donggaem@users.noreply.github.com>
2024-04-09 15:16:15 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.swift
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.swift
@@ -1,32 +1,37 @@
 func solution(_ a:Int, _ b:Int, _ c:Int, _ d:Int) -> Int {
-    let nums = [a, b, c, d]
-        
-    if Set(nums).count == 4 {
-        return nums.min() ?? 0
+  let nums = [a, b, c, d]
+  var countDic = [Int: Int]()
+  
+  for num in nums {
+    countDic[num] = (countDic[num] ?? 0) + 1
+  }
+  
+  switch Set(nums).count {
+  case 1:
+    return nums.first! * 1111
+    
+  case 2:
+    if countDic.values.contains(3) {
+      let p = countDic.first { $0.value == 3 }!.key
+      let q = nums.first { $0 != p }!
+      return (10 * p + q) * (10 * p + q)
+      
+    }else {
+      let p = countDic.keys.first!
+      let q = countDic.keys.dropFirst().first!
+      return (p + q) * abs(p - q)
     }
     
-    var count = [Int: Int]()
+  case 3:
+    let p = countDic.first { $0.value == 2 }!.key
+    let q = nums.first { $0 != p }!
+    let r = nums.first { $0 != p && $0 != q }!
+    return q * r
     
-    for num in nums {
-        count[num] = (count[num] ?? 0) + 1
-    }
+  case 4:
+    return nums.min()!
     
-    if Set(nums).count == 1 {
-        return nums.first! * 1111
-    } else if Set(nums).count == 2 {
-        if count.values.contains(3) {
-            let p = count.first { $0.value == 3 }!.key
-            let q = nums.first { $0 != p }!
-            return (10 * p + q) * (10 * p + q)
-        } else {
-            let p = count.keys.first!
-            let q = count.keys.dropFirst().first!
-            return (p + q) * abs(p - q)
-        }
-    } else {
-        let p = count.first { $0.value == 2 }!.key
-        let q = nums.first { $0 != p }!
-        let r = nums.first { $0 != p && $0 != q }!
-        return q * r
-    }
+  default:
+    return 0
+  }
 }
