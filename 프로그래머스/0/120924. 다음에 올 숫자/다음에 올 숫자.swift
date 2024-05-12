a541faedd6d7010f58b491ab130ec25fdaf3cc09
Donggaem <86482563+Donggaem@users.noreply.github.com>
2024-05-12 18:53:47 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.swift
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.swift
@@ -1,7 +1,7 @@
 func solution(_ common:[Int]) -> Int {
   if (common[1] - common[0]) == (common[2] - common[1]){
-    return common.last! + (common[1] - common[0])
+    return common[common.count - 1] + (common[1] - common[0])
   } else {
-    return common.last! * (common[1] / common[0])
+    return common[common.count - 1] * (common[1] / common[0])
   }
 }
