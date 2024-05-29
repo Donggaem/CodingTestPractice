b2b1a28ed279557a34b30889d3e1e0f1f993b953
Donggaem <86482563+Donggaem@users.noreply.github.com>
2024-05-29 23:41:30 +0900

    [level 1] Title: 실패율, Time: 0.19 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+  var failureRates: [Int : Double] = [:]
+  var player = stages.count
+  for i in 1...N {
+    var noClear = 0
+    
+    for j in 0..<stages.count {
+      if stages[j] == i {
+        noClear += 1
+      }
+    }
+    
+    player -= noClear
+    
+    failureRates[i] = Double(noClear) / Double(player)
+  }
+  
+  return failureRates.sorted(by: <).sorted{$0.value > $1.value}.map{$0.key}
+}
