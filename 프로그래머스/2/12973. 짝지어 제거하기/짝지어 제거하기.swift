55fb4b6b1d4ae26b910c06005ca8c099ea7dff6a
Donggaem <86482563+Donggaem@users.noreply.github.com>
2024-06-04 12:21:47 +0900

    [level 2] Title: 짝지어 제거하기, Time: 353.36 ms, Memory: 24.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ s:String) -> Int{
+  var stack: [Character] = []
+  
+  for i in s {
+    if stack.last == i {
+      stack.removeLast()
+      continue
+    }
+    
+    stack.append(i)
+  }
+  
+  return stack.isEmpty ? 1 : 0
+}
